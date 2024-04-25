c26855282b8f61474e422294c28b62eae13987c9
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-04-25 06:59:18 -0400

    initial commit 2

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, redirect, flash, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "abcdef"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Milagros@localhost/pets'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
